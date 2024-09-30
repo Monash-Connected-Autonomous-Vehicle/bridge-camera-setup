2c1cd094c4b57d149f11373fdc5676154e373329
emily <111032070+emilylkha@users.noreply.github.com>
2024-09-30 15:08:13 +0800

    adding px2_setup.sh

---
+++ px2_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the ROS master URI to the ROS 1 master running on the Docker container
+export ROS_MASTER_URI=http://192.168.1.2:11311
+
+# Set the ROS IP address for the PX2
+export ROS_IP=192.168.1.181
+
+# Navigate to the workspace where the camera launch file is located
+cd ~/Documents/ros_gmsl_ws
+
+# Source the workspace environment
+source devel/setup.bash
+
+# Launch the cameras using roslaunch
+roslaunch gmsl_n_cameras gmsl_n_cameras.launch
